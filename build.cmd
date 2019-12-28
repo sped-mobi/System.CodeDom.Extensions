2fb4387e8479116138ee4fed230c90574bb1d005
Brad Marshall <bmarshall@sped.mobi>
2019-12-28 02:33:14 -0600

    updates

--- build.cmd
+++ build.cmd
@@ -1,11 +1,3 @@
 @echo off
-
-dotnet clean -v m
-
-dotnet restore -v m
-
-dotnet build -v m
-
-for /f "tokens=*" %%i IN ('dir bin\Debug\*.nupkg /s /b') DO (
-dotnet nuget push "%%i" -k d3cb76e3-890c-4874-a882-267e518aee3d -s https://www.myget.org/F/ollon-buildtools/
-)
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
+exit /b %ErrorLevel%
